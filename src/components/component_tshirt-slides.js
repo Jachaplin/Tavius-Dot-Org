a8b1c3ea5539929d2d80b672d2c7583c23709e33
Jachaplin <jakealanchaplin@yahoo.com>
2018-09-19 01:35:37 -0700

    connected antd UI framework

---
+++ src/components/component_tshirt-slides.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Carousel } from 'antd';
+import '../style/tshirt-slides.css';
+
+class TshirtSlides extends Component {
+  render() {
+    return (
+      <Carousel autoplay>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+      </Carousel>
+    );
+  };
+};
+
+export default TshirtSlides;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import './style/index.css';
+import TshirtSlides from './components/component_tshirt-slides';
+// import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-	}
+	// constructor(props) {
+	// 	super(props)
+	// }
 
 	render() {
 		return(
-			<div>Hello World</div>
-			)
+			<div>
+				<TshirtSlides />
+			</div>
+		)
 	}
 }
 
-
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
