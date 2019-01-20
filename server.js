f9e5c962fa739186a951e2e92be8c4955fa35361
Jachaplin <jakealanchaplin@yahoo.com>
2019-01-19 16:49:59 -0800

    deployment setup

--- server.js
+++ server.js
@@ -32,14 +32,14 @@ app.use('/api/user', user);
 app.use('/api/subscribe', subscriber);
 app.use('/api/admin', admin);
 
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+// 	// Set static folder
+// 	app.use(express.static('client/build'));
 
-	app.get('/*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 const port = process.env.PORT || 5000;
 
